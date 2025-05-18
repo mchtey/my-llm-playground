7e096bd58182022e1eb43b9223e8218fb579c6fb
mchtey <mucahit0yilmaz@gmail.com>
2025-05-19 02:28:32 +0300

    Add agentic chunking implementation with proposition extraction

--- Chunking/chunking-strategies-in-rag/app.py
+++ Chunking/chunking-strategies-in-rag/app.py
@@ -143,3 +143,50 @@ text_splitter = SemanticChunker(
 )
 documents = text_splitter.create_documents([text])
 print(documents)
+
+# 5. Agentic Chunking
+print("#### Proposition-Based Chunking ####")
+
+# https://arxiv.org/pdf/2312.06648.pdf
+
+from langchain.output_parsers.openai_tools import JsonOutputToolsParser
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableLambda
+from langchain.chains import create_extraction_chain
+from typing import Optional, List
+from langchain.chains import create_extraction_chain_pydantic
+# from langchain_core.pydantic_v1 import BaseModel
+from langchain import hub
+from pydantic import BaseModel
+
+prompt_template = hub.pull("wfh/proposal-indexing")
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+runnable = prompt_template | llm
+
+
+class Sentences(BaseModel):
+    sentences: List[str]
+
+
+# Extraction
+extraction_chain = create_extraction_chain_pydantic(pydantic_schema=Sentences, llm=llm)
+
+
+def get_propositions(text):
+    runnable_output = runnable.invoke({
+        "input": text
+    }).content
+    propositions = extraction_chain.invoke(runnable_output)["text"][0].sentences
+    return propositions
+
+
+paragraphs = text.split("\n\n")
+text_propositions = []
+for i, para in enumerate(paragraphs[:5]):
+    propositions = get_propositions(para)
+    text_propositions.extend(propositions)
+    print(f"Done with {i}")
+
+print (f"You have {len(text_propositions)} propositions")
+print(text_propositions[:10])
