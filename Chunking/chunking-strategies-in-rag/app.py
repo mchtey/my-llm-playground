9f4ca577385f111b9075fcb2e29a8c32ef0ec5b8
Mücahit Yılmaz <mucahit.yilmaz@ithinka.com>
2025-05-18 18:42:21 +0300

    Add RAG system with LangChain, Chroma, and Ollama

---
+++ Chunking/chunking-strategies-in-rag/app.py
@@ -0,0 +1,35 @@
+from rich import print
+from langchain.docstore.document import Document
+from langchain_community.chat_models import ChatOllama
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import  StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+
+local_llm = ChatOllama(model = "mistral")
+
+# RAG
+def rag(chunks, collection_name):
+    vectorstore = Chroma.from_documents(
+        documents=documents,
+        collection_name=collection_name,
+        embedding=embeddings.ollama.OllamaEmbeddings(model="nomic-embed-text"),
+    )
+    retriever = vectorstore.as_retriever()
+
+    prompt_template = """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(prompt_template)
+
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | local_llm
+        | StrOutputParser()
+    )
+    result = chain.invoke("what is the use of Text Splitting?")
+    print(result)
